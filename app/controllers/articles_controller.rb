f5428dc0b696f7a71204f6a7ad024bb8be668ee5
Roman Alenskiy <kuperman88@gmail.com>
2018-02-03 17:38:55 +0300

    differentiation of access to articles feature completed (user can edit or delete only those articles that he created himself)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,16 @@ class ArticlesController < ApplicationController
 include ArticlesHelper
 
 	before_action :require_login, except: [:show, :index]
+	before_action :access_control, only: [:edit, :destroy]
+	
+	def access_control
+		@article = Article.find(params[:id])
+		@author = current_user
+		if @article.author_id != @author.id
+			redirect_to article_path(@article)
+			flash.notice = "You haven't access! This is not your article!"
+		end
+	end
 	
 	def index
 		@articles = Article.all
@@ -13,6 +23,8 @@ include ArticlesHelper
 	
 	def create
 		@article = Article.new(article_params)
+		@author = current_user
+		@article.author_id = @author.id
 		@article.save
 		redirect_to article_path(@article)
 		
@@ -41,6 +53,7 @@ include ArticlesHelper
 	
 	def show
 		@article = Article.find(params[:id])
+		@author = current_user
 		@comment = Comment.new
 		@comment.article_id = @article.id
 	end

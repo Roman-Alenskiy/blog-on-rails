514480eed9099c70cc090af51a2f691278a16801
romalenskiy <kuperman88@gmail.com>
2019-03-07 20:45:53 +0300

    Fixed vulnerability with raw html in articles body

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,6 @@
 module ArticlesHelper
 	def article_params
+		params[:article][:body] = Sanitize.fragment(params[:article][:body], Sanitize::Config::BASIC)
 		params.require(:article).permit(:title, :body, :tag_list, :image)
 	end
 end
